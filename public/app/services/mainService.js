53a9a1ddd1871bd237bd85b779ade746193dfb00
asemle <asemle@umass.edu>
2017-01-23 12:10:30 -0700

    add straps page

--- public/app/services/mainService.js
+++ public/app/services/mainService.js
@@ -1,9 +1,16 @@
 angular.module("app")
 	.service("mainService", function($http) {
-		this.getWatches = function() {
+		this.getWatches = function(x) {
 			return $http.get('/watches')
 			.then(function(response) {
+				if(x) {
+					return response.data.filter(function(x) {
+						return id === x
+					})
+				}
+				else {
 				return response;
+			}
 			})
 		};
 		this.getStraps = function() {
