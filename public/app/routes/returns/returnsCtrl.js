4ca6df93f9740e871b79abaff2b765fc455da178
olsondeven <olsondeven@gmail.com>
2017-01-24 12:15:33 -0700

    mailer for return, mailCtrl on server side, sweetalert, index.js updates

--- public/app/routes/returns/returnsCtrl.js
+++ public/app/routes/returns/returnsCtrl.js
@@ -1,5 +1,31 @@
 angular.module("app")
-.controller('returnsCtrl', function($scope, $stateParams, mainService, $rootScope) {
+.controller('returnsCtrl', function($scope, $stateParams, mainService, contactSrvc, $rootScope) {
   $rootScope.header = "Returns – Paul Valentine US";
+
+  $scope.send = function(name,email,number,order,message){
+    console.log('returnCtrl fired',name,email,number,order,message);
+    if(!name || !email || !number || !order || !message){
+      return swal('please fill out all fields');
+    }
+    if(!email.match(/@/gi)){
+      return swal('please enter a valid email');
+    }
+    
+    let obj = {
+      name: name,
+      email: email,
+      phone: number,
+      order: order,
+      message: message
+    }
+
+    contactSrvc.send(obj);
+
+    $scope.name = '';
+    $scope.email = '';
+    $scope.number = '';
+    $scope.order = '';
+    $scope.message = '';
+  };
 
 });
